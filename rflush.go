64354ba6080e58ca132d99fb102ce07cdb68624c
Anjula <anjula@Anjulas-MacBook-Pro.local>
2020-06-25 01:26:58 +0530

    New Implementation

--- rflush.go
+++ rflush.go
@@ -1,322 +1,349 @@
-package rflush
+package main
 
-import (
-	"fmt"
-	"math"
-	"sort"
+const (
+	maxEntries = 40
+	minEntries = maxEntries * 40 / 100
 )
 
 type RTree struct {
-	MinChildren float64
-	MaxChildren float64
-	root        node
-	size        int
-	height      int
+	height   int
+	root     BBox
+	count    int
+	reinsert []BBox
 }
 
 // node represents a tree node of an Rtree.
 type node struct {
-	leaf      bool
-	children  []node
-	count     int
-	item	  Item
+	count    int
+	children [maxEntries + 1]BBox
 }
-type Item struct {
-	MinX      float64
-	MinY      float64
-	MaxX      float64
-	MaxY      float64
+//bbox represents a boundary box
+type BBox struct {
+	Min, Max  [2]float64
 	Reference string
-}
-
-// NewTree returns an Rtree.
-func NewRTree(maxEntries int) *RTree {
-	if maxEntries != 0 {
-		maxEntriesNode := math.Max(4, float64(maxEntries))
-		minEntriesNode := math.Max(2, math.Ceil(maxEntriesNode*0.4))
-		return &RTree{
-			MinChildren: minEntriesNode,
-			MaxChildren: maxEntriesNode,
-		}
-	}
-	maxEntriesNode := math.Max(4, 9)
-	minEntriesNode := math.Max(2, math.Ceil(maxEntriesNode*0.4))
-	return &RTree{
-		MinChildren: minEntriesNode,
-		MaxChildren: maxEntriesNode,
-	}
+	Data      interface{}
 }
 
 
-func (r *RTree) Insert(minX, minY, maxX, maxY float64, reference string) {
-	var item node
-	set(minX,minY,maxX,maxY,reference,&item)
-	r.insert(item, r.height - 1, false)
+func (r *RTree) Insert(min, max [2]float64, reference string, value interface{}) {
+	var item BBox
+	set(min,max, reference,value, &item)
+	r.insert(&item)
 }
 
-func (r *RTree) insert(item node, level int, isNode bool) {
-	var bbox node
-	var insertPath []node
-	if isNode{
-		bbox = item
-	}else {
-		bbox = item.toBBox()
+func (r *RTree) insert(item *BBox) {
+	if r.root.Data == nil{
+		set(item.Min,item.Max, item.Reference, new(node), &r.root)
+	}
+	grown := r.root.insert(item, r.height)
+	if grown {
+		r.root.extend(item)
+	}
+	if r.root.Data.(*node).count == maxEntries+1 {
+		newRoot := new(node)
+		r.root.split(&newRoot.children[1])
+		newRoot.children[0] = r.root
+		newRoot.count = 2
+		r.root.Data = newRoot
+		r.root.adjustParentBBoxes()
+		r.height++
+	}
+	r.count++
+
+}
+
+func (b *BBox) insert (item *BBox, height int)(grown bool){
+	n := b.Data.(*node)
+	if height == 0 {
+		n.children[n.count] = *item
+		n.count++
+		grown = !b.contains(item)
+		return grown
 	}
 
-	// find the best node for accommodating the item, saving all nodes along the path too
-	chosenNode := r.root.chooseSubtree(bbox,level,insertPath)
-
-	// put the item into the node
-	chosenNode.children = append(chosenNode.children, item)
-	chosenNode.extend(&bbox)
-
+	// ind the best node index for accommodating the item
+	index := b.chooseSubtree(item)
+	child := &n.children[index]
+	grown = child.insert(item, height-1)
+	if grown {
+		child.extend(item)
+		grown = !b.contains(item)
+	}
 	// split on node overflow; propagate upwards if necessary
-	for level >=0 {
-		if len(insertPath[level].children) > int(r.MaxChildren) {
-			r.split(insertPath,level)
-			level--
-		}else {
-			break
-		}
+	if child.Data.(*node).count == maxEntries+1 {
+		child.split(&n.children[n.count])
+		n.count++
 	}
-	// adjust bboxes along the insertion path
-	r.adjustParentBBoxes(&bbox,insertPath,level)
+	return grown
 }
 
-
-func (n *node) chooseSubtree(bbox node, level int,path []node) (chosenNode *node){
-	for {
-		path = append(path, bbox)
-		if n.leaf || len(path) - 1 == level{
-			break
-		}
-		minEnlargement := 0.0
-		minArea := 0.0
-		var targetNode *node
-
-		for i := 0; i < len(n.children); i++ {
-			child := n.children[i]
-			area := child.bboxArea()
-			enlargement := bbox.enlargedArea(&child) - area
-
-			// choose entry with the least area enlargement
-			if enlargement < minEnlargement {
-				minEnlargement = enlargement
-				if area < minArea {
-					minArea = area
-				}
-				targetNode = &child
-			} else if enlargement == minEnlargement {
-				// otherwise choose one with the smallest area
-				if area < minArea {
-					minArea = area
-					targetNode = &child
-				}
-			}
-		}
-		if targetNode != nil {
-			chosenNode = targetNode
-		}else {
-			chosenNode = &n.children[0]
-		}
-	}
-	return chosenNode
-}
-func (r *RTree ) adjustParentBBoxes(bbox *node, path []node, count int) {
-	// adjust bboxes along the given tree path
-	for i := count; i >= 0; i-- {
-		path[i].extend(bbox)
-	}
-}
-
-func set(minX, minY, maxX, maxY float64, reference string, bbox *node){
-	bbox.item.MinX = minX
-	bbox.item.MinY = minY
-	bbox.item.MaxX = maxX
-	bbox.item.MaxY = maxY
-	bbox.item.Reference = reference
-}
-
-func (n node) toBBox() node{
-	return n
-}
-func (n *node) bboxArea() float64 {
-	return (n.item.MaxX - n.item.MinX) * (n.item.MaxY - n.item.MinY)
-}
-
-func (n *node) enlargedArea(t *node) float64 {
-	return (math.Max(t.item.MaxX, n.item.MaxX) - math.Min(t.item.MinX, n.item.MinX)) *
-		(math.Max(t.item.MaxY, n.item.MaxY) - math.Min(t.item.MinY, n.item.MinY))
-}
-
-func (n *node) extend(b *node) {
-	n.item.MinX = math.Min(n.item.MinX, b.item.MinX)
-	n.item.MinY = math.Min(n.item.MinY, b.item.MinY)
-	n.item.MaxX = math.Max(n.item.MaxX, b.item.MaxX)
-	n.item.MaxY = math.Max(n.item.MaxY, b.item.MaxY)
-}
-
-// split overflowed node into two
-func (r *RTree) split(insertPath []node, level int) {
-	node := insertPath[level]
-	M := len(node.children)
-	m := int(r.MinChildren)
-
-	r.chooseSplitAxis(&node, m, M)
-
-	//splitIndex := r.chooseSplitIndex(&node, m, M)
-	//
-	//newNode := createNode(node.children[:splitIndex])
-	//newNode.count = node.count
-	//newNode.leaf = node.leaf
-	//
-	//node.calcBBox()
-	//newNode.calcBBox()
-	//
-	//if level != 0 {
-	//	insertPath[level-1].children = append(insertPath[level-1].children, newNode)
-	//} else {
-	//	r.splitRoot(node, newNode)
-	}
-
-}
-
-func (r *RTree) splitRoot(oldNode, newNode node) {
-	var children []node
-	children = append(children, oldNode, newNode)
-	r.root = createNode(children)
-	r.height = oldNode.count + 1
-	r.root.leaf = false
-	r.root.calcBBox()
-}
-
-func (r *RTree) chooseSplitAxis(checkNode *node, m, M int) {
-
-	xMargin := allDistMargin(checkNode, m, M, 1)
-	yMargin := allDistMargin(checkNode, m, M, 2)
-
-	// if total distributions margin value is minimal for x, sort by minX,
-	// otherwise it's already sorted by minY
-	if xMargin < yMargin {
-		sort.SliceStable(checkNode.children, func(i, j int) bool {
-			return checkNode.children[i].item.MinX < checkNode.children[j].item.MinX
-		})
-	}
-}
-
-
-// total margin of all possible split distributions where each node is at least m full
-func allDistMargin(checkNode *node, m, M int, compare int) float64 {
-
-	if compare == 1 {
-		sort.SliceStable(checkNode.children, func(i, j int) bool {
-			return checkNode.children[i].item.MinX < checkNode.children[j].item.MinX
-		})
-	} else if compare == 2 {
-		sort.SliceStable(checkNode.children, func(i, j int) bool {
-			return checkNode.children[i].item.MinY < checkNode.children[j].item.MinY
-		})
-	}
-
-	leftBBox := distBBox(checkNode, 0, m, nil)
-	rightBBox := distBBox(checkNode, M-m, M, nil)
-	margin := bboxMargin(&leftBBox) + bboxMargin(&rightBBox)
-
-	for i := m; i < M-m; i++ {
-		child := checkNode.children[i]
-		leftBBox.extend(&child)
-		margin += bboxMargin(&leftBBox)
-	}
-
-	for i := M - m - 1; i >= m; i-- {
-		child := checkNode.children[i]
-		rightBBox.extend(&child)
-		margin += bboxMargin(&rightBBox)
-	}
-	return margin
-}
-
-
-
-func distBBox(checkNode *node, k, p int, destNode *node) node {
-	var destiNode node
-	if destNode == nil {
-		destiNode = createNode(nil)
-	}
-
-	for i := k; i < p; i++ {
-		child := checkNode.children[i]
-		destiNode.extend(&child)
-	}
-	return destiNode
-}
-
-func bboxMargin(a *node) float64 {
-	return (a.item.MaxX - a.item.MinX) + (a.item.MaxY - a.item.MinY)
-}
-
-func createNode(children []node) node {
-	return node{
-		leaf:     true,
-		children: children,
-	}
-}
-
-func (r *RTree) chooseSplitIndex(checkNode *node, m, M int) int {
-	var index int
-	minOverlap := 0.0
+func (b *BBox) chooseSubtree(b2 *BBox) int {
+	k := -1
+	minEnlargement := 0.0
 	minArea := 0.0
-	for i := m; i <= M-m; i++ {
-		bbox1 := distBBox(checkNode, 0, i, nil)
-		bbox2 := distBBox(checkNode, i, M, nil)
-		overlap := bbox1.intersectionArea(&bbox2)
-		area := bbox1.bboxArea() + bbox2.bboxArea()
-
-		// choose distribution with minimum overlap
-		if overlap < minOverlap {
-			minOverlap = overlap
-			index = i
-
+	n := b.Data.(*node)
+	for i := 0; i < n.count; i++ {
+		child := n.children[i]
+		area := child.bboxArea()
+		enlargement := child.enlargedArea(b2) - area
+		// choose entry with the least area enlargement
+		if k == -1 || enlargement < minEnlargement {
+			k = i
+			minEnlargement = enlargement
+			minArea = area
+		} else if enlargement == minEnlargement {
+			// otherwise choose one with the smallest area
 			if area < minArea {
+				k = i
+				minEnlargement = enlargement
 				minArea = area
-			}
-
-		} else if overlap == minOverlap {
-			// otherwise choose distribution with minimum area
-			if area < minArea {
-				minArea = area
-				index = i
 			}
 		}
 	}
-	if index != 0 {
-		return index
+	return k
+}
+
+func (b *BBox) bboxArea() float64 {
+	return (b.Max[0] - b.Min[0]) * (b.Max[1] - b.Min[1])
+}
+
+func (b *BBox) enlargedArea(b2 *BBox) float64 {
+	area := 1.0
+	if b2.Max[0] > b.Max[0] {
+		if b2.Min[0] < b.Min[0] {
+			area *= b2.Max[0] - b2.Min[0]
+		} else {
+			area *= b2.Max[0] - b.Min[0]
+		}
 	} else {
-		return M - m
+		if b2.Min[0] < b.Min[0] {
+			area *= b.Max[0] - b2.Min[0]
+		} else {
+			area *= b.Max[0] - b.Min[0]
+		}
 	}
+	if b2.Max[1] > b.Max[1] {
+		if b2.Min[1] < b.Min[1] {
+			area *= b2.Max[1] - b2.Min[1]
+		} else {
+			area *= b2.Max[1] - b.Min[1]
+		}
+	} else {
+		if b2.Min[1] < b.Min[1] {
+			area *= b.Max[1] - b2.Min[1]
+		} else {
+			area *= b.Max[1] - b.Min[1]
+		}
+	}
+	return area
 }
 
-func (n *node) intersectionArea(b *node) float64 {
-	minX := math.Max(n.item.MinX, b.item.MinX)
-	minY := math.Max(n.item.MinY, b.item.MinY)
-	maxX := math.Min(n.item.MaxX, b.item.MaxX)
-	maxY := math.Min(n.item.MaxY, b.item.MaxY)
-
-	return math.Max(0, maxX-minX) *
-		math.Max(0, maxY-minY)
+func set(min,max [2]float64, reference string, value interface{}, bbox *BBox) {
+	bbox.Min = min
+	bbox.Max = max
+	bbox.Reference = reference
+	bbox.Data = value
 }
 
-// calculate node's bbox from bboxes of its children
-func (n *node) calcBBox() {
-	distBBox(n, 0, len(n.children), n)
-}
-
-func (n *node) contains(b *node) bool{
-	if b.MinX < n.MinX || b.MaxX > n.MaxX {
+// contains return struct when b is fully contained inside of n
+func (b *BBox) contains(b2 *BBox) bool{
+	if b2.Min[0] < b.Min[0] || b2.Max[0] > b.Max[0] {
 		return false
 	}
-	if b.MinY < n.MinY || b.MaxY > n.MaxY {
+	if b2.Min[1] < b.Min[1] || b2.Max[1] > b.Max[1] {
 		return false
 	}
 	return true
+}
+
+
+func (b *BBox) extend(b2 *BBox) {
+	if b2.Min[0] < b.Min[0] {
+		b.Min[0] = b2.Min[0]
+	}
+	if b2.Max[0] > b.Max[0] {
+		b.Max[0] = b2.Max[0]
+	}
+	if b2.Min[1] < b.Min[1] {
+		b.Min[1] = b2.Min[1]
+	}
+	if b2.Max[1] > b.Max[1] {
+		b.Max[1] = b2.Max[1]
+	}
+}
+
+func (b *BBox) split(right *BBox) {
+	axis, _ := b.chooseSplitAxis()
+	left := b
+	leftNode := left.Data.(*node)
+	rightNode := new(node)
+	right.Data = rightNode
+
+	var equals []BBox
+	for i := 0; i < leftNode.count; i++ {
+		minDist := leftNode.children[i].Min[axis] - left.Min[axis]
+		maxDist := left.Max[axis] - leftNode.children[i].Max[axis]
+		if minDist < maxDist {
+			// stay left
+		} else {
+			if minDist > maxDist {
+				// move to right
+				rightNode.children[rightNode.count] = leftNode.children[i]
+				rightNode.count++
+			} else {
+				// move to equals, at the end of the left array
+				equals = append(equals, leftNode.children[i])
+			}
+			leftNode.children[i] = leftNode.children[leftNode.count-1]
+			leftNode.children[leftNode.count-1].Data = nil
+			leftNode.count--
+			i--
+		}
+	}
+	for _, b := range equals {
+		if leftNode.count < rightNode.count {
+			leftNode.children[leftNode.count] = b
+			leftNode.count++
+		} else {
+			rightNode.children[rightNode.count] = b
+			rightNode.count++
+		}
+	}
+	left.adjustParentBBoxes()
+	right.adjustParentBBoxes()
+}
+
+func (b *BBox) chooseSplitAxis() (axis int, size float64) {
+	if b.Max[1]-b.Min[1] > b.Max[0]-b.Min[0] {
+		return 1, b.Max[1] - b.Min[1]
+	}
+	return 0, b.Max[0] - b.Min[0]
+}
+
+func (b *BBox) adjustParentBBoxes() {
+	n := b.Data.(*node)
+	b.Min = n.children[0].Min
+	b.Max = n.children[0].Max
+	for i := 1; i < n.count; i++ {
+		b.extend(&n.children[i])
+	}
+}
+
+// Search ...
+func (r *RTree) Search (
+	min, max [2]float64,
+	iter func(min, max [2]float64,reference string) bool,
+) {
+	var target BBox
+	set(min, max, "",nil, &target)
+	r.search(&target, iter)
+}
+
+func (r *RTree) search (
+	target *BBox,
+	iter func(min, max [2]float64,reference string) bool,
+) {
+	if r.root.Data == nil {
+		return
+	}
+	if target.intersects(&r.root) {
+		r.root.search(target, r.height, iter)
+	}
+}
+
+func (b *BBox) intersects(a *BBox) bool {
+	if a.Min[0] > b.Max[0] || a.Max[0] < b.Min[0] {
+		return false
+	}
+	if a.Min[1] > b.Max[1] || a.Max[1] < b.Min[1] {
+		return false
+	}
+	return true
+}
+
+
+func (b *BBox) search(
+	target *BBox, height int,
+	iter func(min, max [2]float64,reference string) bool,
+) bool {
+	n := b.Data.(*node)
+	if height == 0 {
+		for i := 0; i < n.count; i++ {
+			child := n.children[i]
+			if target.intersects(&child) {
+				if !iter(child.Min, child.Max,child.Reference) {
+					return false
+				}
+			}
+		}
+	} else if height == 1 {
+		for i := 0; i < n.count; i++ {
+			child := n.children[i]
+			if target.intersects(&child) {
+				cn := child.Data.(*node)
+				for i := 0; i < cn.count; i++ {
+					if target.intersects(&cn.children[i]) {
+						if !iter(cn.children[i].Min, cn.children[i].Max,child.Reference) {
+							return false
+						}
+					}
+				}
+			}
+		}
+	} else {
+		for i := 0; i < n.count; i++ {
+			child := n.children[i]
+			if target.intersects(&child) {
+				if !child.search(target, height-1, iter) {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+// All function returns all the entries in the tree.
+func (r *RTree) All() []BBox {
+	if r.root.Reference == "" {
+		return nil
+	}
+	all := r.root.all(r.height)
+	return all
+}
+
+func (b *BBox) all(height int) []BBox {
+	var all []BBox
+	n := b.Data.(*node)
+	if height == 0 {
+		for i := 0; i < n.count; i++ {
+			all = append(all, n.children[i])
+		}
+	} else if height == 1 {
+		for i := 0; i < n.count; i++ {
+			cn := n.children[i].Data.(*node)
+			for j := 0; j < cn.count; j++ {
+				all = append(all, cn.children[i])
+			}
+		}
+	} else {
+		for i := 0; i < n.count; i++ {
+			n.children[i].all(height - 1)
+		}
+	}
+	return all
+}
+
+// Bounds returns the minimum bounding rect
+func (r *RTree) Bounds() (min, max [2]float64) {
+	if r.root.Reference == "" {
+		return
+	}
+	return r.root.Min, r.root.Max
+}
+
+// Len returns the number of items in the tree
+
+func (r *RTree) Len() int{
+	return r.count
 }
--- rflush_test.go
+++ rflush_test.go
@@ -1,3 +1,42 @@
-package rflush
+package main
 
+import (
+	"testing"
+)
 
+//func TestRTree_Insert(t *testing.T) {
+//	var tr RTree
+//
+//	tr.Insert([2]float64{6.969658, 79.873308}, [2]float64{6.969658, 79.873308}, "Cargo Logistics",nil) //0.22km
+//	tr.Insert([2]float64{6.971322, 79.874468}, [2]float64{6.971322, 79.874468}, "Cargills",nil)
+//	tr.Insert([2]float64{6.970960, 79.873652}, [2]float64{6.970960, 79.873652}, "Vystwyke",nil) //0.1km
+//	tr.Insert([2]float64{6.973969, 79.876183}, [2]float64{6.973969, 79.876183}, "Xarena",nil) //cannot 0.35km
+//	tr.Insert([2]float64{6.973597, 79.869424}, [2]float64{6.973597, 79.869424}, "Sea",nil) //Cannot //0.61 km
+//	tr.Insert([2]float64{6.970263, 79.874112}, [2]float64{6.970263, 79.874112}, "Buhari",nil) //0.12km
+//	tr.Insert([2]float64{6.967660, 79.872217}, [2]float64{6.967660, 79.872217}, "Laughs",nil) //0.48km
+//
+//}
+
+func TestRTree_Search(t *testing.T) {
+		var tr RTree
+
+		tr.Insert([2]float64{6.969658, 79.873308}, [2]float64{6.969658, 79.873308}, "Cargo Logistics",nil) //0.22km
+		tr.Insert([2]float64{6.971322, 79.874468}, [2]float64{6.971322, 79.874468}, "Cargills",nil)
+		tr.Insert([2]float64{6.970960, 79.873652}, [2]float64{6.970960, 79.873652}, "Vystwyke",nil) //0.1km
+		tr.Insert([2]float64{6.973969, 79.876183}, [2]float64{6.973969, 79.876183}, "Xarena",nil) //cannot 0.35km
+		tr.Insert([2]float64{6.973597, 79.869424}, [2]float64{6.973597, 79.869424}, "Sea",nil) //Cannot //0.61 km
+		tr.Insert([2]float64{6.970263, 79.874112}, [2]float64{6.970263, 79.874112}, "Buhari",nil) //0.12km
+		tr.Insert([2]float64{6.967660, 79.872217}, [2]float64{6.967660, 79.872217}, "Laughs",nil) //0.48km
+
+		var point [2]float64
+		point[0] = 6.973597
+		point[1] = 79.869424
+		var locationNames []interface{}
+		tr.Search(point,point, func(min, max [2]float64,reference string) bool{
+			locationNames = append(locationNames,reference)
+			return false
+		},)
+		if len(locationNames) != 1{
+			t.Error("Search Function Test: Failed")
+		}
+}
